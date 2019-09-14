733a5603b947e12db88772ed49d42519514270dd
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-09-14 17:00:22 +0200

    Add buttons and import styles

--- src/Comment.js
+++ src/Comment.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import './Comment.css';
 
-const Comment = ({text, votes}) => 
-	<li>{text} <span>votes: {votes}</span></li>;
+const Comment = ({text, votes, id, thumbUpComment, thumbDownComment, removeComment}) =>
+    <li className="Comment">
+        {text} 
+        <div className="Votes">votes: {votes}</div>
+        <div className="Buttons">
+            <button onClick={() => thumbUpComment(id)}>+</button>
+            <button onClick={() => thumbDownComment(id)}>-</button>
+            <button onClick={() => removeComment(id)}>x</button>
+        </div>
+    </li>
 
 export default Comment;
