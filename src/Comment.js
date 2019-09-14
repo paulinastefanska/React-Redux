f5142a9ef4f3323aa381a3ca7e70551e23f863d5
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-09-14 17:15:22 +0200

    Change vote name

--- src/Comment.js
+++ src/Comment.js
@@ -4,7 +4,7 @@ import './Comment.css';
 const Comment = ({text, votes, id, thumbUpComment, thumbDownComment, removeComment}) =>
     <li className="Comment">
         {text} 
-        <div className="Votes">votes: {votes}</div>
+        <div className="Votes">Votes: {votes}</div>
         <div className="Buttons">
             <button onClick={() => thumbUpComment(id)}>+</button>
             <button onClick={() => thumbDownComment(id)}>-</button>
