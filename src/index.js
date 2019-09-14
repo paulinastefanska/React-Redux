a891825466c1a563afafbf5ff57bbc77702d5998
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-09-14 12:11:15 +0200

    Add import reducer

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { reducer } from './reducer';
 
 const store = createStore(reducer);
 
