03077af533be02a353a64060a53e915841f5ba03
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-09-14 12:58:41 +0200

    Add store dispatch

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import { reducer } from './reducer';
+import reducer from './reducer';
+import addComment from './actions';
 
 const store = createStore(reducer);
 
@@ -16,8 +17,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+store.dispatch(addComment("First comment"));
+store.dispatch(addComment("Second comment"));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
