f389261cfb8ca3cfbc3b5df683c6df1f8f3cbf96
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-09-14 15:08:35 +0200

    Add and import NewCommentContainer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import CommentsListContainer from './CommentsListContainer';
+import NewCommentContainer from './NewCommentContainer';
 
 const App = () => {
   return (
     <div className="App">
       <h1>Comments App</h1>
       <CommentsListContainer />
+      <h2>Add new comment</h2>
+      <NewCommentContainer />
     </div>
   );
 };
